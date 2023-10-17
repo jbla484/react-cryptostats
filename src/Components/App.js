27551397d31eb151ec09e9b955a8e9bcde2b4cc7
Jbla484 <jbla484@wgu.edu>
2023-10-16 18:20:03 -0700

    added navbar, fixed bugs with table

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,17 @@
-import Coins from "./Coins";
-import "../Styles/App.css";
+import Coins from './Coins';
+import '../Styles/App.css';
 
 export default function App() {
-  return (
-    <div className="App-header">
-      <h1>Crypto Coin Statistics</h1>
-      <div className="coinHeader border_radius">
-        <div className="coinContainer">
-          <Coins />
+    return (
+        <div className='App-header'>
+            <nav>
+                <h4>CryptoStats</h4>
+            </nav>
+            <div className='coinHeader border_radius'>
+                <div className='coinContainer'>
+                    <Coins />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/Components/Coins.jsx
+++ src/Components/Coins.jsx
@@ -1,41 +1,44 @@
-import { useState, useEffect } from "react";
-import TableHeader from "./TableHeader";
-import Coin from "./Coin";
-import "../Styles/Coins.css";
+import { useState, useEffect } from 'react';
+import TableHeader from './TableHeader';
+import Coin from './Coin';
+import '../Styles/Coins.css';
 
 export default function Coins() {
-  const [coins, setCoins] = useState([]);
+    const [coins, setCoins] = useState([]);
 
-  //https://documenter.getpostman.com/view/5734027/RzZ6Hzr3?version=latest
-  //https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=USD
+    //https://documenter.getpostman.com/view/5734027/RzZ6Hzr3?version=latest
+    //https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=USD
 
-  useEffect(() => {
-    getCoins();
-    setInterval(() => {
-      getCoins();
-    }, 55000);
-  }, []);
+    useEffect(() => {
+        getCoins();
+        setInterval(() => {
+            getCoins();
+        }, 30000);
+    }, []);
 
-  const getCoins = async function () {
-    let response = await fetch(
-      `https://api.coinstats.app/public/v1/coins?skip=0&currency=USD`
+    const getCoins = async function () {
+        let response = await fetch(
+            `https://api.coinstats.app/public/v1/coins?skip=0&currency=USD`
+        );
+        let data = await response.json();
+        setCoins(data.coins);
+    };
+
+    return (
+        <table>
+            <TableHeader />
+            <tbody>
+                {coins.length > 0 && (
+                    <>
+                        {coins.map((coin) => (
+                            <Coin
+                                key={coin.id}
+                                coin={coin}
+                            />
+                        ))}
+                    </>
+                )}
+            </tbody>
+        </table>
     );
-    let data = await response.json();
-    setCoins(data.coins);
-  };
-
-  return (
-    <table>
-      <TableHeader />
-      <tbody>
-        {coins.length > 0 && (
-          <>
-            {coins.map((coin) => (
-              <Coin key={coin.id} coin={coin} />
-            ))}
-          </>
-        )}
-      </tbody>
-    </table>
-  );
 }
